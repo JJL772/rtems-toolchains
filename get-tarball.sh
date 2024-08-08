333a0f88b27762daa4e052839da9c195e850a88a
JJL772 <jeremy.lorelli.1337@gmail.com>
2024-08-08 15:51:45 -0700

    Move to /opt/rtems/<version> to more closely match how EPICS does it for RTEMS 4.X/5

--- get-tarball.sh
+++ get-tarball.sh
@@ -2,10 +2,15 @@
 
 set -e
 
+VERSION=6
 while test $# -gt 0; do
 	case $1 in
 		-t)
 			TAG=$2;
+			shift 2
+			;;
+		-v)
+			VERSION=$2
 			shift 2
 			;;
 		*)
@@ -19,4 +24,4 @@ if [ -z "$TAG" ]; then
 	exit 1
 fi
 
-docker run --rm -u $(id -u):$(id -g) -v "$PWD":"$PWD" -w "$PWD" ghcr.io/jjl772/rtems-6:$TAG tar -C /usr/local -czvf rtems-6-$TAG.tar.gz .
+docker run --rm -u $(id -u):$(id -g) -v "$PWD":"$PWD" -w "$PWD" ghcr.io/jjl772/rtems-$VERSION:$TAG tar -C /opt/rtems/$VERSION -czvf rtems-$VERSION-$TAG.tar.gz .
